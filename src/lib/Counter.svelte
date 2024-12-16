a80da3e4ec97705336eb3270669973e29e89562e
YOUR <YOUR>
2024-12-16 21:20:59 +0800

    Temp Converter Added

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,18 @@
+<script>
+    let count = $state(0);
+    let doubled = $derived(count * 2);
+
+    function onclick(){
+        count++;
+    }
+</script>
+
+<div>
+    Clicked {count} times! That is {doubled} when doubled.
+</div>
+<button {onclick}>Click!</button>
+
+<style>
+
+
+</style>
---
+++ src/lib/TempConverter.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let inputC = $state(0);
+    let inputF = $state(32);
+
+    function calcConv(i: number, t: string){
+        return Math.round((t=="F" ? (i * (9/5) + 32) : ((i - 32) * (5/9))) * 10) / 10
+    }
+</script>
+
+C <input type="number" onkeyup={() => (inputF = calcConv(inputC, "F"))} bind:value={inputC}> - F <input type="number" onkeyup={() => (inputC = calcConv(inputF, "C"))} bind:value={inputF}>
+
+
+
+<style>
+
+</style>
